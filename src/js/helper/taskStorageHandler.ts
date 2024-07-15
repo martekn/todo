3fe7d1cad1990bda98be1a075f8049534a5a5ac8
martekn <marte.mk@hotmail.com>
2024-07-15 17:41:08 +0200

    Add a clear storage function

--- src/js/helper/taskStorageHandler.ts
+++ src/js/helper/taskStorageHandler.ts
@@ -9,7 +9,7 @@ export const taskStorageHandler = {
    *
    * @returns {Task[]} The list of tasks stored in localStorage. If no tasks are found, an empty array is returned.
    */
-  get() {
+  get(): Task[] {
     const tasksJson = localStorage.getItem("tasks") ?? "[]";
     const tasks: Task[] = JSON.parse(tasksJson);
 
@@ -22,5 +22,11 @@ export const taskStorageHandler = {
    */
   set(tasks: Task[]) {
     localStorage.setItem("tasks", JSON.stringify(tasks));
+  },
+  /**
+   * Clears the list of tasks in localStorage
+   */
+  clear() {
+    localStorage.setItem("tasks", JSON.stringify([]));
   }
 };
