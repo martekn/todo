482d47688d8e59cc0673bb54bc30011f6b0261c5
martekn <marte.mk@hotmail.com>
2024-07-16 21:11:45 +0200

    Cleanup jsdoc

--- src/js/types/task.ts
+++ src/js/types/task.ts
@@ -1,20 +1,11 @@
 /**
  * Represents a task in the to-do list.
+ * @property id - The unique identifier for the task.
+ * @property title - The title or description of the task.
+ * @property completed - Indicates whether the task has been completed.
  */
 export interface Task {
-  /**
-   * The unique identifier for the task.
-   */
   id: string;
-
-  /**
-   * The title or description of the task.
-   */
   title: string;
-
-  /**
-   * Indicates whether the task has been completed.
-   * @type {boolean}
-   */
   completed: boolean;
 }
