de8f0a2cdd340d8830208956389563e0b481b5af
martekn <marte.mk@hotmail.com>
2024-07-16 18:25:21 +0200

    Load tasks and setup event listeners

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,2 +1,24 @@
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@/scss/main.scss";
+import { safeQuerySelector } from "@/js/helper/safeQuerySelector";
+import { addTask } from "@/js/handlers/addTask";
+import { clearTasks } from "@/js/handlers/clearTasks";
+import { loadTasks } from "./helper/loadTasks";
+
+loadTasks();
+
+try {
+  // Create task form submit event listener
+  const button = safeQuerySelector<HTMLButtonElement>("#clear-button");
+  button.addEventListener("click", clearTasks);
+
+  // Clear all tasks event listener
+  const form = safeQuerySelector<HTMLFormElement>("form");
+  form.addEventListener("submit", addTask);
+} catch (error) {
+  if (error instanceof Error) {
+    console.error(error.message);
+  } else {
+    console.error("Caught an unknown error", error);
+  }
+}
